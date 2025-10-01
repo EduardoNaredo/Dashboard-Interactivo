dcf966fbf048355ecddc0886b6f0f74461cd3e83
EduardoNaredo <A01734935@tec.mx>
2025-10-01 17:55:33 -0600

    Funcion suma

--- Bienvenida.py
+++ Bienvenida.py
@@ -1,1 +1,13 @@
 print("Este es un archivo que nos dará la bienvenida")
+
+print("Hola, bienvenido a mi programa")
+
+a = 89
+b = 52
+c = 23
+
+def suma():
+    print("La suma es:", a + b + c)
+
+    suma()
+    
