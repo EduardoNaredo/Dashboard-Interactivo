5b7a7537cc4d7d0e3aca0aec5e541ada88f54415
EduardoNaredo <A01734935@tec.mx>
2025-10-01 18:44:44 -0600

    creacion de funciones de backend

--- Bienvenida.py
+++ Bienvenida.py
@@ -10,4 +10,21 @@ def suma():
     print("La suma es:", a + b + c)
 
     suma()
-    
+
+import matplotlib.pyplot as plt
+
+def graficar_ventas(dias, ventas):
+    plt.figure(figsize=(8, 4))
+    plt.plot(dias, ventas, marker='o', linestyle = "-", color = "blue")
+    plt.title("Ventas Diarias")
+    plt.xlabel("Días")
+    plt.ylabel("Ventas")
+    plt.grid(True)
+    plt.show()
+
+def main_frontend():
+    dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"]
+    ventas = [150, 200, 250, 300, 400]
+    graficar_ventas(dias, ventas)
+
+main_frontend()
